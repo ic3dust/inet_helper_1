c18abb7fe9699f0cd56f688542a9c2a8a7f66de7
ic3dust <canadskiyakkaunt1@gmail.com>
2024-05-28 20:21:52 +0300

    Style fixes + copy button addition

--- subs.js
+++ subs.js
@@ -2,8 +2,24 @@ function Function1() {
     let quotesFix = document.querySelector("#quotesFix").value;
     quotesFix = quotesFix.replace(/«/g, '"');
     quotesFix = quotesFix.replace(/»/g, '"');
-    let a = " \nКопіюйте впевнено, блокноту начхати на фон тексту";
-    let mas = [quotesFix, a];
-    document.querySelector("#message1out").textContent = mas;
-    return mas;
+    document.querySelector("#message1out").textContent = quotesFix;
+    return quotesFix;
+}
+function copyText() {
+    const textToCopy = document.getElementById('message1out').innerText;
+    const tempTextArea = document.createElement('textarea');
+    tempTextArea.value = textToCopy;
+    document.body.appendChild(tempTextArea);
+    tempTextArea.select();
+    tempTextArea.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+    document.body.removeChild(tempTextArea);
+    success();
+}
+function success() {
+    let message = document.getElementById('copy_message');
+    message.style.opacity = 1; 
+    setTimeout(() => {
+         message.style.opacity = 0; 
+    }, 2000);
 }
